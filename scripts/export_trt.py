a608ab937c937088533f63e43fe43960284e690f
yanbc <imyanbc@gmail.com>
2022-11-23 04:41:41 +0800

    update profile settings in scripts/export_trt.py

--- scripts/export_trt.py
+++ scripts/export_trt.py
@@ -1,6 +1,6 @@
 import os
 import argparse
-from typing import Tuple
+from typing import Tuple, List
 import tensorrt as trt
 import onnx
 
@@ -9,6 +9,24 @@ import onnx
 # meta
 ##############
 _WORKSPACE = 8<<30
+# !!! require manual setting for now
+_PROFILES = [
+    {
+        "input": ((1, 3, 512, 512), (1, 3, 512, 512), (9, 3, 512, 512))
+    },
+    {
+        "input": ((1, 3, 512, 384), (1, 3, 512, 384), (9, 3, 512, 384))
+    },
+    {
+        "input": ((1, 3, 384, 512), (1, 3, 384, 512), (9, 3, 384, 512))
+    },
+    {
+        "input": ((1, 3, 512, 320), (1, 3, 512, 320), (9, 3, 512, 320))
+    },
+    {
+        "input": ((1, 3, 320, 512), (1, 3, 320, 512), (9, 3, 320, 512))
+    },
+]
 
 
 ##############
@@ -17,26 +35,14 @@ _WORKSPACE = 8<<30
 def build_engine(onnx_file_path,
                 engine_file_path,
                 engine_name,
+                optimization_profiles: List[dict],
                 use_fp16=True,
-                dynamic_shapes={},
                 workspace_size=_WORKSPACE
     ):
-    """Build TensorRT Engine
-
-    :use_fp16: set mixed flop computation if the platform has fp16.
-    :dynamic_shapes: {binding_name: (min, opt, max)}, default {} represents not using dynamic.
-    :dynamic_batch_size: set it to 1 if use fixed batch size, else using max batch size
-    """
-    use_dynamic_shapes = len(dynamic_shapes) > 0
     trt_logger = trt.Logger(trt.Logger.VERBOSE)
     builder = trt.Builder(trt_logger)
 
     # create network
-    # if use_dynamic_shapes:
-    #     network = builder.create_network(
-    #         1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
-    # else:
-    #     network = builder.create_network()
     network = builder.create_network(
         1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH))
     network.name = engine_name
@@ -50,13 +56,14 @@ def build_engine(onnx_file_path,
 
     # set tensor shapes
     # builder.max_batch_size = batch_size
-    if use_dynamic_shapes:
-        profile = builder.create_optimization_profile()
-        for binding_name, dynamic_shape in dynamic_shapes.items():
-            min_shape, opt_shape, max_shape = dynamic_shape
-            profile.set_shape(
-                binding_name, min_shape, opt_shape, max_shape)
-        config.add_optimization_profile(profile)
+    if len(optimization_profiles) > 0:
+        for op in optimization_profiles:
+            builder_profile = builder.create_optimization_profile()
+            for binding_name, shapes in op.items():
+                min_shape, opt_shape, max_shape = shapes
+                builder_profile.set_shape(
+                    binding_name, min_shape, opt_shape, max_shape)
+            config.add_optimization_profile(builder_profile)
 
     # parse ONNX
     parser = trt.OnnxParser(network, trt_logger)
@@ -112,7 +119,7 @@ def parse_TensorShapeProto(shape: onnx.onnx_ml_pb2.TensorShapeProto) -> Tuple[in
 def get_args():
     p = argparse.ArgumentParser()
     p.add_argument("onnxpath", help="Path to onnx model file")
-    p.add_argument("--batchsize", type=int, default=1, help="Batch size. Defualt to 1.")
+    # p.add_argument("--batchsize", type=int, default=1, help="Batch size. Defualt to 1.")
     p.add_argument("--enginepath", default="res.engine", help="Where to save tensorrt engine file. Default to res.engine")
     return p.parse_args()
 
@@ -120,28 +127,18 @@ def get_args():
 if __name__ == "__main__":
     args = get_args()
     onnx_path = args.onnxpath
-    batch_size = args.batchsize
     engine_path = args.enginepath
+    profiles = _PROFILES
 
     # parse onnx file
     dtype_map = get_onnx_dtype_map()
     onnx_graph = onnx.load(onnx_path).graph
     model_name = onnx_graph.name
-    dynamic_shapes = dict()
-    if batch_size != 1:
-        for input_io in onnx_graph.input:
-            io_name = input_io.name
-            io_dtype = dtype_map[input_io.type.tensor_type.elem_type]
-            io_shape = parse_TensorShapeProto(input_io.type.tensor_type.shape)
-            io_shape = io_shape[1:]     # remove leading batch
-            dynamic_shapes[io_name] = [
-                (1, *io_shape), (batch_size, *io_shape), (batch_size, *io_shape)
-            ]
 
     build_engine(
         onnx_path,
         engine_path,
         model_name,
-        dynamic_shapes=dynamic_shapes,
+        optimization_profiles=profiles,
         use_fp16=True
     )
