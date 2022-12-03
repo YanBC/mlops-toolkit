ade37b4e0c480ef29a624888c987a8fcd91ca2c1
yanbc <imyanbc@gmail.com>
2022-12-03 09:59:16 +0800

    multi-profile trt_info.py

--- scripts/trt_info.py
+++ scripts/trt_info.py
@@ -1,124 +1,52 @@
-import sys
-from typing import List, Tuple
-from dataclasses import dataclass
-import tensorrt as trt
+import argparse
+from utils.trt_profile import query_profiles, load_engine, Profile
 
 
-@dataclass
-class Binding:
-    name: str
-    dtype: trt.DataType
-    shape: tuple
-    size: int
-    nbytes: int
-    isInput: bool
+def is_dynamic_profile(profile: Profile) -> bool:
+    input_dynamic = [b.isDynamic for b in profile.inputs]
+    return any(input_dynamic)
 
 
-@dataclass
-class Profile:
-    idx: int
-    binding_name: str
-    min_shape: Tuple[int]
-    opt_shape: Tuple[int]
-    max_shape: Tuple[int]
-
-
-def get_binding_info(engine: trt.ICudaEngine) -> List[Binding]:
-    num = engine.num_bindings
-    max_batch_size = engine.max_batch_size
-    bindings = []
-    for idx in range(num):
-        name = engine.get_binding_name(idx)
-        dtype = engine.get_binding_dtype(idx)
-        shape = engine.get_binding_shape(idx)
-        size = abs(trt.volume(shape)) * max_batch_size
-        nbytes = size * dtype.itemsize
-        isInput = engine.binding_is_input(idx)
-        binding = Binding(name, dtype, shape, size, nbytes, isInput)
-        bindings.append(binding)
-    return bindings
-
-
-def load_engine(engine_file_path: str) -> trt.ICudaEngine:
-    trt_logger = trt.Logger(trt.Logger.ERROR)
-    # Force init TensorRT plugins
-    trt.init_libnvinfer_plugins(None, '')
-    with open(engine_file_path, "rb") as f, \
-            trt.Runtime(trt_logger) as runtime:
-        engine = runtime.deserialize_cuda_engine(f.read())
-    return engine
-
-
-def get_engine_info(engine: trt.ICudaEngine) -> str:
+def get_engine_info(engine_path: str) -> str:
+    profiles = query_profiles(engine_path)
+    engine = load_engine(engine_path)
     name = engine.name
-    # max_batch_size = engine.max_batch_size    # DEPRECATED
-    num_bindings = engine.num_bindings
-    bindings = get_binding_info(engine)
-    engine_size = engine.device_memory_size
-    input_host_mem_size = 0
-    input_device_mem_size = 0
-    output_host_mem_size = 0
-    output_device_mem_size = 0
-    for b in bindings:
-        if b.isInput:
-            input_device_mem_size += b.nbytes
-            input_host_mem_size += b.nbytes
-        else:
-            output_device_mem_size += b.nbytes
-            output_host_mem_size += b.nbytes
-    host_mem_size = input_host_mem_size + output_host_mem_size
-    device_mem_size = input_device_mem_size + output_device_mem_size
 
-    num_optimization_profiles = engine.num_optimization_profiles
-    num_inputs_per_profile = \
-            len([b for b in bindings if b.isInput]) // num_optimization_profiles
-
-    profiles = []
-    profile_idx = 0
-    input_count = 0
-    profile = dict()
-    for binding in bindings:
-        if binding.isInput:
-            binding_name = binding.name
-            min_shape, opt_shape, max_shape = engine.get_profile_shape(profile_idx, binding_name)
-            profile[binding_name] = Profile(
-                    profile_idx,
-                    binding_name,
-                    min_shape,
-                    opt_shape,
-                    max_shape
-            )
-            input_count += 1
-        if input_count == num_inputs_per_profile:
-            profiles.append(profile)
-            input_count = 0
-            profile_idx += 1
-            profile = dict()
+    default_profile = profiles[0]
+    num_bindings_per_profile = len(default_profile.inputs) + len(default_profile.outputs)
+    num_input_bindings = len(default_profile.inputs)
+    num_output_bindings = len(default_profile.outputs)
+    input_names = [b.name for b in default_profile.inputs]
+    output_names = [b.name for b in default_profile.outputs]
+    input_binding_idx = [b.idx for b in default_profile.inputs]
+    output_binding_idx = [b.idx for b in default_profile.outputs]
 
     ret_str = ""
     ret_str += f"Model name: {name}\n"
-    ret_str += f"Number of bindings: {num_bindings}\n"
-    for idx, b in enumerate(bindings):
-        ret_str += f"    #{idx} {b}\n"
-    ret_str += f"Number of optimization profiles: {num_optimization_profiles}\n"
+    ret_str += f"Number of bindings per profile: {num_bindings_per_profile}\n"
+    ret_str += f"    num of inputs: {num_input_bindings}\n"
+    for idx, name in zip(input_binding_idx, input_names):
+        ret_str += f"        idx: {idx}, name: {name}\n"
+    ret_str += f"    num of outputs: {num_output_bindings}\n"
+    for idx, name in zip(output_binding_idx, output_names):
+        ret_str += f"        idx: {idx}, name: {name}\n"
+
+    ret_str += f"Number of optimization profiles: {len(profiles)}\n"
     for idx, p in enumerate(profiles):
-        ret_str += f"    profile #{idx}\n"
-        for input_name in p:
-            ret_str += f"        {p[input_name]}\n"
-    # ret_str += f"Max batch size: {max_batch_size}\n"  # DEPRECATED
-    ret_str += f"Memory footprints:\n"
-    ret_str += f"    Device memory: {device_mem_size/2**20:.2f} MiB\n"
-    # ret_str += f"        Engine: {engine_size/2**20:.2f} MiB\n"
-    ret_str += f"        Input:  {input_device_mem_size/2**20:.2f} MiB\n"
-    ret_str += f"        Output: {output_device_mem_size/2**20:.2f} MiB\n"
-    ret_str += f"    Host memory: {host_mem_size/2**20:.2f} MiB\n"
-    ret_str += f"        Input:  {input_host_mem_size/2**20:.2f} MiB\n"
-    ret_str += f"        Output: {output_host_mem_size/2**20:.2f} MiB\n"
+        ret_str += f"    profile #{idx}, is_dynamic: {is_dynamic_profile(p)}\n"
+        for input_binding in p.inputs:
+            ret_str += f"        {input_binding}\n"
     return ret_str
 
 
+def get_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("engine", help="Path to tensorrt engine file")
+    return p.parse_args()
+
+
 if __name__ == "__main__":
-    engine_path = sys.argv[1]
-    engine = load_engine(engine_path)
-    info = get_engine_info(engine)
+    args = get_args()
+    engine_path = args.engine
+    info = get_engine_info(engine_path)
     print(info)
---
+++ utils/trt_profile.py
@@ -0,0 +1,149 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+import tensorrt as trt
+import pycuda.driver as cuda
+
+
+def load_engine(engine_file_path: str) -> trt.ICudaEngine:
+    trt_logger = trt.Logger(trt.Logger.ERROR)
+    # Force init TensorRT plugins
+    trt.init_libnvinfer_plugins(None, '')
+    with open(engine_file_path, "rb") as f, \
+            trt.Runtime(trt_logger) as runtime:
+        engine = runtime.deserialize_cuda_engine(f.read())
+    return engine
+
+
+def save_engine(engine_file_path: str, engine: trt.ICudaEngine):
+    with open(engine_file_path, 'wb') as f:
+        f.write(engine.serialize())
+
+
+@dataclass
+class Binding:
+    idx: int                    # engine.get_binding_shape
+    name: str                   # engine.get_binding_shape
+    dtype: trt.DataType
+    min_shape: Tuple[int]
+    opt_shape: Tuple[int]
+    max_shape: Tuple[int]       # output binding only has max_shape
+    isInput: bool
+    isDynamic: bool
+
+
+def get_nbytes(shape: Tuple[int], dtype: trt.DataType) -> int:
+    size = abs(trt.Volume(shape))
+    nbytes = size * dtype.itemsize
+    return nbytes
+
+
+def get_cuda_mem_nbytes(binding: Binding) -> int:
+    max_nbytes = get_nbytes(binding.max_shape, binding.dtype)
+    return max_nbytes
+
+
+def isDynamic(shape: Tuple[int]) -> bool:
+    dynamic = [d == -1 for d in shape]
+    return any(dynamic)
+
+
+@dataclass
+class Profile:
+    idx: int
+    inputs: List[Binding]
+    outputs: List[Binding]
+
+
+def infer_output_max_shape(profile: Profile, engine: trt.ICudaEngine):
+    profile_idx = profile.idx
+    context = engine.create_execution_context()
+    stream = cuda.Stream()
+    try:
+        context.set_optimization_profile_async(profile_idx, stream.handle)
+        stream.synchronize()
+
+        for binding in profile.inputs:
+            context.set_binding_shape(binding.idx, binding.max_shape)
+        assert context.all_shape_inputs_specified
+
+        for binding in profile.outputs:
+            binding.max_shape = context.get_binding_shape(binding.idx)
+    finally:
+        del stream
+        del context
+
+
+def get_profiles(engine: trt.ICudaEngine) -> List[Profile]:
+    num_bindings = engine.num_bindings
+    num_profiles = engine.num_optimization_profiles
+    num_bindings_per_profile = num_bindings // num_profiles
+
+    ret_profiles = []
+    count = 0
+    profile_idx = 0
+    input_bindings = []
+    output_bindings = []
+    for binding_idx in range(num_bindings):
+        name = engine.get_binding_name(binding_idx)
+        dtype = engine.get_binding_dtype(binding_idx)
+        shape: trt.Dims = engine.get_binding_shape(binding_idx)
+        shape: Tuple[int] = tuple(shape)
+        if engine.binding_is_input(binding_idx):
+            min_shape, opt_shape, max_shape = engine.get_profile_shape(profile_idx, binding_idx)
+            isInput = True
+        else:
+            min_shape = [-1]
+            opt_shape = [-1]
+            max_shape = [-1]
+            isInput = False
+
+        binding = Binding(
+                idx=binding_idx,
+                name=name,
+                dtype=dtype,
+                min_shape=min_shape,
+                opt_shape=opt_shape,
+                max_shape=max_shape,
+                isInput=isInput,
+                isDynamic=isDynamic(shape)
+        )
+
+        if isInput:
+            input_bindings.append(binding)
+        else:
+            output_bindings.append(binding)
+
+        count += 1
+        if count == num_bindings_per_profile:
+            profile = Profile(
+                idx=profile_idx,
+                inputs=input_bindings,
+                outputs=output_bindings,
+            )
+            ret_profiles.append(profile)
+
+            count = 0
+            profile_idx += 1
+            input_bindings = []
+            output_bindings = []
+
+    for profile in ret_profiles:
+        infer_output_max_shape(profile, engine)
+    return ret_profiles
+
+
+#################
+# main
+#################
+def query_profiles(engine_path: str, gpu_id: int = 0) -> List[Profile]:
+    cuda.init()
+    cuda_ctx = cuda.Device(gpu_id).make_context()
+    engine = None
+    try:
+        engine = load_engine(engine_path)
+        profiles = get_profiles(engine)
+        return profiles
+    finally:
+        if engine is not None:
+            del engine
+        cuda_ctx.pop()
